63c8eab9c32b637642cc8e745985a613705adf32
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-11-02 23:16:23 -0400

    Variable mistake

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ export default function Home() {
                 <ReCharCard name={'Professor Cogsworth'} description={'Professor Cogsworth is a master of clockwork engineering and a distinguished gentleman'} image={char2path} />
                 <ReCharCard name={'Colonel Rustbottom'} description={'Colonel Rustbottom is a retired military strategist turned eccentric inventor'} image={char3path} />
                 <ReCharCard name={'Captain Gearheart'} description={'Captain Gearheart is a charismatic and adventurous airship captain'} image={char4path} />
-                <ReCharCard name={'Lady Clockwork'} description={'Lady Clockwork is a brilliant and enigmatic inventor with an eye for detail'} image={char1path} />
+                <ReCharCard name={'Lady Clockwork'} description={'Lady Clockwork is a brilliant and enigmatic inventor with an eye for detail'} image={char5path} />
             </div>
 		</div>
     )
