f54508f3854023931253021509d85e8fa76b5913
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-11-03 05:28:30 -0500

    Added Logo in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,28 @@
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
+import logoPath from '../assets/videos/sp-logo.mp4'
 
 export default function Navbar() {
-  return (
-    <nav className="flex items-center justify-between p-4 border-b border-orange-500 bg-black">
-      {/* Left Side: GIF Image */}
-      <div className="flex-shrink-0">
-        <img src="path_to_logo.gif" alt="Logo" className="h-8 w-8" />
-      </div>
+	return (
+		<nav className="fixed top-0 left-0 w-full z-50 flex items-center justify-between p-4 border-b border-orange-500 bg-black opacity-90">
+			<div className="flex-shrink-0">
+				<video src={logoPath} alt="Logo" className="h-8 w-8" />
+			</div>
 
-      {/* Center: Website Name with Glitter Effect */}
-      <div className="flex-grow text-center">
-        <h1 className="text-xl text-center font-bold glitter-text"><Link to="/">Steam Punk</Link></h1>
-      </div>
+			<div className="flex-grow text-center">
+				<h1 className="text-xl text-center font-bold glitter-text">
+					<Link to="/">Steam Punk</Link>
+				</h1>
+			</div>
 
-      {/* Right Side: Buttons with Glitter Effect */}
-      <div className="flex space-x-4">
-        <Link
-          to="/play-game"
-          className="px-4 py-2 font-semibold rounded hover:bg-orange-600 glitter-button"
-        >
-          Play Game
-        </Link>
-        <Link
-          to="/chatbot"
-          className="px-4 py-2 font-semibold rounded hover:bg-orange-600 glitter-button"
-        >
-          StreamPunk Chat
-        </Link>
-      </div>
-    </nav>
-  );
+			<div className="flex space-x-4">
+				<Link to="/play-game" className="px-4 py-2 font-semibold rounded hover:bg-orange-600 glitter-button">
+					Play Game
+				</Link>
+				<Link to="/chatbot" className="px-4 py-2 font-semibold rounded hover:bg-orange-600 glitter-button">
+					StreamPunk Chat
+				</Link>
+			</div>
+		</nav>
+	);
 }
