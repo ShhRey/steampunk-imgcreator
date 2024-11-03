78a9ccae36ce3718831fa6c5e7e6edd6d34a2c18
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-11-03 01:36:45 -0400

    Changing CSS as per Tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Navbar from '../components/Navbar.jsx';
 import '../styles/Home.css';
-import arenaVideo from '../assets/videos/arena.mp4';
+// import arenaVideo from '../assets/videos/arena.mp4';
 import ReCharCard from '../components/ReCharCard.jsx';
 import char1path from '../assets/images/char1.png';
 import char2path from '../assets/images/char2.png';
@@ -11,29 +11,17 @@ import char5path from '../assets/images/char5.png';
 export default function Home() {
 	
     return (
-        <div className="home">
+        <div id='home' className="flex flex-col">
             <Navbar />
-            <video className='backgroundVideo' autoPlay loop muted src={arenaVideo} type="video/mp4" />
-            <div className="overlay">
-                <div className='leftContent'>
-                    <h1>SteamPunk SSD</h1>
-                    <h2>NJIT's annual beginner-friendly 24 hour hackathon hosted by its ACM chapter.</h2>
-                    <h3>Who Can Participate?</h3>
-                    <div className="highCards">
-                        <div className="card1"></div>
-                        <div className="card2"></div>
-                        <div className='card3'></div>
-                    </div>
-                    <div className='descText'>
-                        <h3>Hackathon Rules</h3>
-                        <ol style={{ listStyleType: 'decimal' }}>
-                            <li className="listText1">Above legal age of majority in country of residence</li>
-                            <li className="listText2">All countries/territories, excluding standard exceptions</li>
-                            <li className="listText3">College students only</li>
-                        </ol>
+            <div id='heroContent' className="flex flex-row">
+                <div id='heroLeftCont' className='flex flex-col'>
+                    <div id='descTextCont' className='flex flex-col'>
+                        <h2>Some Tagline for Project</h2>
+                        <h3>Some Description about this Project</h3>
                     </div>
                 </div>
             </div>
+            
             <div className='horScrollContainer'>
                 <ReCharCard name={'Gadgeteer Garvin'} description={'Gadgeteer Garvin is an eccentric inventor and skilled engineer'} image={char1path} />
                 <ReCharCard name={'Professor Cogsworth'} description={'Professor Cogsworth is a master of clockwork engineering and a distinguished gentleman'} image={char2path} />
